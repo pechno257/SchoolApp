71b177763844d2b587acd716c1d7a5b1d69a00b1
Jean Petit <altoch@hotmail.com>
2018-09-06 22:31:51 -0400

    Added EnglishPaper class

---
+++ SchoolLibrary/EnglishPaper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolLibrary
+{
+    class EnglishPaper
+    {
+        public string Title { get; set; }
+        public int MinimumWordCount { get; set; }
+        public string PaperText { get; set; }
+    }
+}
